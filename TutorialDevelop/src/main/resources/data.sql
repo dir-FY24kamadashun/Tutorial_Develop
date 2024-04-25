21890c9ede3d84aec355fc1131532f947a2a68c5
dir-FY24kamadashun <shun.kamada.f332@dirbato.co.jp>
2024-04-25 18:10:35 +0900

    最初のコミット

---
+++ TutorialDevelop/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO user(name,gender,age,email)
+VALUES("きらめき太郎","男性",27,'taro.kirameki@mail.com');
+INSERT INTO user(name,gender,age,email)
+VALUES("きらめき次郎","男性",22,'jiro.kirameki@mail.com');
+INSERT INTO user(name,gender,age,email)
+VALUES("きらめき花子","女性",25,'hanako.kirameki@mail.com')
